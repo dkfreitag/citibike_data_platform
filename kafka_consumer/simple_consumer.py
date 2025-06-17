70fc951168fa31b56bc8d1948d7a2e396ae635af
David Freitag <dkfreitag@gmail.com>
2025-06-17 13:25:58 -0400

    updating to use ECR

--- kafka_consumer/simple_consumer.py
+++ kafka_consumer/simple_consumer.py
@@ -6,10 +6,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def main():
-    consumer = KafkaConsumer('station-status', bootstrap_servers=f'{os.getenv('BROKER_PRIVATE_IP_ADDRESS')}:9092')
+    # consumer = KafkaConsumer('station-status', bootstrap_servers=f'{os.getenv('BROKER_PRIVATE_IP_ADDRESS')}:9092')
 
-    for message in consumer:
-        print(message.value)
+    # for message in consumer:
+    #     print(message.value)
+    for i in range(20):
+        print(i)
 
 if __name__ == '__main__':
     main()
