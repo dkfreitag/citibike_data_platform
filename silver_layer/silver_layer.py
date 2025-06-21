ae4aa854f66c9d876a5509b1fb3395a12ae6f8d2
David Freitag <dkfreitag@gmail.com>
2025-06-20 20:58:22 -0400

    saving spark job after success

--- silver_layer/silver_layer.py
+++ silver_layer/silver_layer.py
@@ -19,9 +19,19 @@ job.init(args['JOB_NAME'], args)
 station_status_df = spark.read.json('s3://citibike-data-platform-project-bucket/kafka_output/*')
 station_information_df = spark.read.json('s3://citibike-data-platform-project-bucket/station_information/*')
 
-full_station_df = station_status_df.join(broadcast(station_information_df), station_status_df.station_id == station_status_df.station_id)
+# rename columns
+station_status_df_renamed = station_status_df.withColumnRenamed("last_updated", "last_updated_station_status") \
+                                             .withColumnRenamed("version", "version_station_status")
+station_information_df_renamed = station_information_df.withColumnRenamed("last_updated", "last_updated_station_information") \
+                                                       .withColumnRenamed("version", "version_station_information") \
+                                                       .withColumnRenamed("station_id", "station_id_station_information") \
+                                                       
+full_station_df = station_status_df_renamed.join(broadcast(station_information_df_renamed), station_information_df_renamed.station_id_station_information == station_status_df_renamed.station_id)
+
+# drop join column since we already have it
+write_df = full_station_df.drop('station_id_station_information')
 
 # Write as Parquet to S3
-full_station_df.write.mode('overwrite').parquet('s3://citibike-data-platform-project-bucket/silver_layer/')
+write_df.write.mode('overwrite').parquet('s3://citibike-data-platform-project-bucket/silver_layer/')
 
 job.commit()
